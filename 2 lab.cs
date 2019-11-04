747d89935b452336b56b31b155fe85aed1f63be9
ryzhuk01 <ryzhuk001@mail.ru>
2019-11-05 01:16:39 +0300

    lab 2,3

---
+++ 2 lab.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double a4 = 15.7;
+            int a = 7;
+            char с = 'А';
+            float f = 0.5f;
+            
+
+            //явное преобор
+            int a5 = 11;
+ 
+            byte a6 = (byte)a5;
+            byte a7 = Convert.ToByte(a5);
+            sbyte a8 = Convert.ToSByte(a5);
+
+
+
+            //неявное
+
+            double a9 = a5;
+            float a10 = a5;
+            long a11 = a5;
+
+            //упаковка
+            Int32 x = 5;
+            Object o = x;
+
+            // распаковка
+            int s9 = (int)o;
+
+
+            int? xx = null;
+            Nullable<int> zz = null;
+
+
+
+
+
+            string name = "Evgeny";
+            Console.WriteLine("My name is {0} ", name);
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine("\n compare, contains, substring, insert, replace\n");
+            string name1 = "My name is Evgeny";
+            string name2 = "Evgeny";
+            string str = "123";
+            Console.WriteLine(String.Compare(name, name2));
+            Console.WriteLine(String.Compare(str, name));
+            Console.WriteLine(name1.Contains(name));
+            Console.WriteLine(name1.Substring(3));
+            Console.WriteLine(name1.Insert(11, "Ryzhuk "));
+            Console.WriteLine(name1.Replace("Evgeny" , "John"));
+
+
+
+            string s1 = "asd";
+            string s2 = "";
+            string s3 = null;
+            Console.WriteLine("\n is null or empty \n");
+            Console.WriteLine(string.IsNullOrEmpty(s1));
+            Console.WriteLine(string.IsNullOrEmpty(s2));
+            Console.WriteLine(string.IsNullOrEmpty(s3));
+
+
+            //kортежи
+
+
+            (int, int) tuple = (5, 3);
+            Func(tuple.Item1, tuple.Item2);
+            void Func(int l, int p)
+            {
+                int res = l + p;
+                Console.WriteLine($"Sum = {res}");
+            }
+            
+
+            void checked_t()
+            {
+                int vr = 1686312345;
+                int vs = 343499999;
+                int vc = unchecked(vr + vs);
+                Console.WriteLine(vc);
+            }
+            checked_t();
+
+        }
+    }
+}
---
+++ 3 lab.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab3
+{
+    public partial class customer
+    {
+        public int number, balance;
+        private int id;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        
+           public static int count = 0;
+        public string surname, name;
+        readonly string address;
+
+        public object[] arr;
+        public override string ToString()
+        {
+            return (" Имя :" + name + " номер : " + number+ " баланс :" + balance);
+        }
+        public override bool Equals(object obj)
+
+        {
+            customer tmp = obj as customer;
+            if (this.name == tmp.name)
+                return true;
+            else return false;
+        }
+        public override int GetHashCode()
+        {
+            return count + 1;
+        }
+    }
+
+    public partial class customer
+    {
+        public  int k = 0;
+        public customer(string a, string b, int c, int d, int e, string f)
+        {
+            Console.WriteLine(" constructor with parametrs");
+            name = a;
+            surname = b;
+            number = c;
+            balance = d;
+            id = GetHashCode();
+            address = f;
+            count++;
+        }
+
+        public customer()
+        {
+            Console.WriteLine(" constructor withour parametrs");
+            id = 0;
+            number = 0;
+            balance = 0;
+            surname = "";
+            name = "";
+            address = "";
+            count++;
+        }
+        private customer(int z, int v)
+        {
+            Console.WriteLine(" constructor");
+            id = z;
+            balance = v;
+        }
+        static customer()
+        {
+
+            Console.WriteLine(" static construtor; the first customer was created");
+        }
+      
+
+
+    }
+    public partial class customer
+    {
+
+        // зачисление
+        public void zachisl(int f)
+        {
+            balance += f;
+            Console.WriteLine("zichisleno {0} , balance = {1}", f, this.balance);
+
+        }
+        // списание
+        public void spisanie(int k)
+        {
+            if (this.balance < k)
+            {
+                Console.WriteLine("nedostatochno sredstv");
+            }
+            else
+            {
+                this.balance -= k;
+
+                Console.WriteLine("Spisano {0} , balance = {1}", k, this.balance);
+            }
+        }
+
+        
+    }
+
+    class Cmin
+    {
+        public static int min(int x, int y)
+        {
+            int z = (x < y) ? x : y;
+            return z;
+        }
+        public static int minabs(ref int x, ref int y)
+        {
+            x = (x < 0) ? -x : x; y = (y < 0) ? -y : y;
+            int z = (x < y) ? x : y;
+            return z;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            customer[] arr = new customer[3];
+
+            arr[0] = new customer("kirill", "naibich", 400, 100, 1, "bereza");
+            arr[1] = new customer("sanya", "prudilko", 200, 120, 2, "shchuchin");
+            arr[2] = new customer("ilya", "smirnov", 150, 110, 3, "osipovichi");
+        
+
+
+            arr[0].zachisl(100);
+            arr[1].spisanie(100);
+            int x = 100;
+            int x2 = 200;
+            foreach (customer a in arr)
+
+                if (a.number > x && a.number < x2)
+                    Console.WriteLine(a.ToString());
+
+            int с = -4;
+            int b = 2;
+            Console.WriteLine("a={0}  b={1}", с, b);
+            int k = Cmin.min(с, b);
+            Console.WriteLine("a={0}  b={1}", с, b);
+            Console.WriteLine("k=" + k);
+            k = Cmin.minabs(ref с, ref b);
+            Console.WriteLine("a={0}  b={1}", с, b);
+            Console.WriteLine("k=" + k);
+
+        }
+
+    }
+  
+
+    
+} 
+
+
+
