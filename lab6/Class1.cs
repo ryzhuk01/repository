765a3ecfe2770f5b6b88c59e433f54c886207b40
ryzhuk01 <ryzhuk001@mail.ru>
2019-11-26 15:08:02 +0300

    lab6

---
+++ lab6/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    partial class Product
+    {
+        public override bool Equals(object obj)
+        {
+            Product product = (Product)obj;
+            return ((this.OrganizationName == product.OrganizationName) && (this.ProductName == product.ProductName));
+        }
+
+        public override int GetHashCode()
+        {
+            return this.OrganizationName.GetHashCode() + this.ProductName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.OrganizationName + " " + this.ProductName;
+        }
+    }
+    enum list
+    {
+        one = 1,
+        two,
+        three,
+        nine = 9,
+        ten
+    }
+    public class warehouse
+    {
+        public Base[] elems;
+        public int count = 0;
+        public int size;
+        list list = new list();
+        public warehouse()
+        {
+            size = 100;
+            elems = new Base[100];
+        }
+
+
+        public warehouse(int c)
+        {
+            size = c;
+            elems = new Base[c];
+        }
+
+
+        public bool isFull()
+        {
+
+            return (count == size);
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+
+
+        public void Add(Base el)
+        {
+            if (isFull())
+                return;
+            elems[count++] = el;
+        }
+
+
+        public void Del(Base el)
+        {
+            int num = 0;
+            if (isEmpty())
+                return;
+            for (int i = 0; i < count; i++)
+            {
+                if (elems[i].Equals(el))
+                    num = i;
+            }
+            for (int i = num; i < count; i++)
+            {
+                elems[i] = elems[i + 1];
+            }
+            count--;
+        }
+
+
+        public void showByOrg(warehouse warehouse,string org)
+        {
+            
+            for (int i = 0; i < count; i++)
+            {
+                if (warehouse.elems[i].OrganizationName == org)
+                    Console.WriteLine(elems[i].ToString());
+            }
+            Console.WriteLine();
+        }
+
+
+        public void show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(elems[i].ToString());
+            }
+            Console.WriteLine();
+        }
+      
+    }
+
+
+
+    public abstract class Base
+    {
+        public struct Info
+        {
+            public int price;
+            public int mas;
+        }
+        public Info info = new Info();
+    }
+
+
+    public static class controller
+    {
+       
+        public static void sort(warehouse warehouse)
+        {
+            Base elem;
+            for (int i = 0; i < warehouse.count - 1; i++)
+            {
+                for (int j = 0; j < warehouse.count - i - 1; j++)
+                {
+                    if ((warehouse.elems[j].info.mas)/(warehouse.elems[j].info.price) < (warehouse.elems[j+1].info.mas) / (warehouse.elems[j+1].info.price))
+                    {
+                        elem = warehouse.elems[j];
+                        warehouse.elems[j] = warehouse.elems[j + 1];
+                        warehouse.elems[j + 1] = elem;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace lab6
+{
+    interface IMyInterface
+    {
+        string MyFunc();
+    }
+
+    interface IAnotherInterface
+    {
+        string ToString();
+    }
+
+    partial class Product : Base, IAnotherInterface
+    {
+        public string OrganizationName { get; set; }
+        public string ProductName { get; set; }
+
+        public Product()
+        {
+            OrganizationName = "NoName organization";
+            ProductName = "NoName product";
+        }
+
+        public Product(string organization, string product, int price, int mas)
+        {
+            OrganizationName = organization;
+            ProductName = product;
+            info.price = price;
+            info.mas = mas;
+        }
+
+       
+    }
+
+    class Furniture : Product, IAnotherInterface
+    {
+        public string FurnitureType { get; set; }
+        public Furniture() : base()
+        {
+            FurnitureType = "NoName FurnitureType";
+        }
+
+        public Furniture(string organization, string product, string furnituretype, int price, int mas) : base(organization, product, price, mas)
+        {
+            FurnitureType = furnituretype;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.FurnitureType;
+        }
+    }
+
+    abstract class cupboard : Furniture, IAnotherInterface
+    {
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.name;
+        }
+
+        public cupboard() : base()
+        {
+            name = "NoName cupboard";
+        }
+
+        public cupboard(string organization, string product, string SweetsType, string Name, int price, int mas) : base(organization, product, SweetsType, price, mas)
+        {
+            name = Name;
+        }
+
+        public abstract string MyFunc();
+    }
+
+    sealed class Bed : Furniture, IAnotherInterface
+    {
+        public string BedType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.BedType;
+        }
+
+        public Bed() : base()
+        {
+            BedType = "NoName Filling";
+        }
+
+        public Bed(string organization, string product, string manufacturer, string bedtype, int price, int mas) : base(organization, product, manufacturer, price, mas)
+        {
+            BedType = bedtype;
+        }
+    }
+
+    class Hanger : Product, IAnotherInterface
+    {
+        public string HangerCoast { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.HangerCoast;
+        }
+
+        public Hanger() : base()
+        {
+            HangerCoast = "price is not set";
+        }
+
+        public Hanger(string organization, string product, string manufacturer, string coast, int price, int mas) : base(organization, product,price,mas)
+        {
+            HangerCoast = coast;
+        }
+    }
+
+    class closet : cupboard, IMyInterface, IAnotherInterface
+    {
+        public string Resolution { get; set; }
+        public override string MyFunc()
+        {
+            return "Переопределение";
+        }
+        string IMyInterface.MyFunc()
+        {
+            return "Реализация метода интерфейса";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Resolution;
+        }
+    }
+
+
+
+    class chair : Furniture, IAnotherInterface
+    {
+        public string ChairType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.ChairType;
+        }
+
+        public chair() : base()
+        {
+            ChairType = "NoType";
+        }
+
+        public chair(string organization, string product, string manufacturer, string chairtype,int price, int mas) : base(organization, product, manufacturer,price,mas)
+        {
+            ChairType = chairtype;
+        }
+    }
+    class sofa : Furniture, IAnotherInterface
+    {
+        public string SofaType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.SofaType;
+        }
+
+        public sofa() : base()
+        {
+            SofaType = "NoType";
+        }
+
+        public sofa(string organization, string product, string manufacturer, string sofatype,int price,int mas) : base(organization, product, manufacturer,price,mas)
+        {
+            SofaType = sofatype;
+        }
+    }
+
+    class Print
+    {
+        public static void IAmPrinting(IAnotherInterface obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("org1", "prod1",200,700);
+            Furniture furn = new Furniture("org2", "prod2", "furniture1",600,2000);
+            closet closet1 = new closet();
+            warehouse warehouse1 = new warehouse(10);
+            closet1.info.price = 300;
+            closet1.info.mas = 3000;
+            warehouse1.Add(product);
+            warehouse1.Add(furn);
+            warehouse1.Add(closet1);
+            warehouse1.Add(new Furniture("org2", "prod4", "furniture2", 1000, 1500));
+            warehouse1.Add(new Product("org1", "prod5", 400, 200));
+
+            warehouse1.show();
+           
+            controller.sort(warehouse1);
+            warehouse1.show();
+            warehouse1.Del(closet1);
+            warehouse1.show();
+           
+        }
+    }
+}
